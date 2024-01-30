7b0973e63b5d620685fd582fd01d09a3e3c880dc
darshangoswami <goswamidarshan999@gmail.com>
2024-01-30 16:35:33 -0500

    generated key for encryption/decryption

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+from cryptography.fernet import Fernet
+
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)'''
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+key = load_key()
 master_pwd = input("Please enter the master password: ")
 
 def view():
